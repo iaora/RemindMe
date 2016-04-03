5d599cd63a82aa38c8c27ec3b93681b4e06cd1ea
Jamie Liao <j.liao5079@gmail.com>
2016-04-03 10:59:47 -0400

    WEEE

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request, render_template
 from pymongo import MongoClient
 import datetime
 from datetime import timedelta
+import twilio.twiml
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 client = MongoClient()
 db = client.reminders
+textClient = TwilioRestClient(app.config["ACCOUNT_SID"], app.config["AUTH_TOKEN"])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,16 +18,38 @@ def index():
         message = request.form['message']
         offset = request.form['offset']
 
+        if number is None or message is None or offset is None:
+            return render_template('error.html')
+
         end_time = datetime.datetime.utcnow() + timedelta(minutes=int(offset))
 
         db.test.insert_one({'number': number,
                             'date': end_time,
                             'message': message})
 
-        print "yay it added"
         return render_template('index.html',number=number, message=message)
 
     return render_template('index.html')
+
+@app.route('/message', methods=['POST'])
+def reply():
+    if request.method == 'POST':
+        body = request.form["Body"]
+        word = body.split()
+
+        end_time = datetime.datetime.utcnow() + timedelta(minutes=int(word[1]))
+        text = word[2]
+        for i in range (3,len(word)):
+            text = text + " " +  word[i]
+            print text
+
+        db.test.insert_one({'number': word[0],
+                            'date': end_time,
+                            'message': text})
+
+        resp = twilio.twiml.Response()
+        return str(resp.message("Success!"))
+
 
 if __name__ == '__main__':
     app.debug=True
--- check.py
+++ check.py
@@ -1,21 +1,27 @@
-from twilio.rest import TwilioRestClient
+from flask import render_template
 from pymongo import MongoClient
-from app import app
+from app import app, textClient
 import time, datetime
+import twilio
 
 db = MongoClient().reminders
-textClient = TwilioRestClient(app.config["ACCOUNT_SID"], app.config["AUTH_TOKEN"])
+#textClient = TwilioRestClient(app.config["ACCOUNT_SID"], app.config["AUTH_TOKEN"])
 
-TIME_BREAK = 10
+TIME_BREAK = 50
 
 while(True):
     query = {
         'date' : { "$lte": datetime.datetime.utcnow() }
         }
     for expired in db.test.find(query):
-        textClient.messages.create(to='+1'+expired['number'],
-                                from_='+18623079011',
-                                body=expired['message'])
+        try:
+            textClient.messages.create(to='+1'+expired['number'],
+                                    from_='+18623079011',
+                                    body=expired['message'])
+        except twilio.TwilioRestException as e:
+            return render_template('error.html')
+
+
         db.test.remove(expired)
 
 
